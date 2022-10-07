b0d59381c6969df0f683d4efcab379d4d2ee37dc
merpich <merpich.dev@gmail.com>
2022-10-07 11:27:47 +0300

    Продолжение верстки

--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
 function App() {
 	return (
 		<div className="wrapper">
+			<div style={{display: 'none'}} className="overlay">
+				<div className="drawer">
+					<div className="drawer__header">
+						<h2 className="drawer__title">Корзина</h2>
+						<button className="drawer__button">
+							<svg className="drawer__icon" width={20} height={20}>
+								<use xlinkHref="/img/icons/sprites.svg#plus"></use>
+							</svg>
+						</button>
+					</div>
+
+					<div className="cart">
+						<article className="cart-item">
+							<img className="cart-item__image" width={70} height={70} src="/img/sneakers/1.jpg" alt="Sneakers" />
+							<div className="cart-item__info">
+								<h4 className="cart-item__title">Мужские Кроссовки Nike Air Max 270</h4>
+								<span className="cart-item__price">12 999 руб.</span>
+							</div>
+							<button className="cart-item__button">
+								<svg className="cart-item__icon" width={11} height={11}>
+									<use xlinkHref="/img/icons/sprites.svg#plus"></use>
+								</svg>
+							</button>
+						</article>
+						<article className="cart-item">
+							<img className="cart-item__image" width={70} height={70} src="/img/sneakers/1.jpg" alt="Sneakers" />
+							<div className="cart-item__info">
+								<h4 className="cart-item__title">Мужские Кроссовки Nike Air Max 270</h4>
+								<span className="cart-item__price">12 999 руб.</span>
+							</div>
+							<button className="cart-item__button">
+								<svg className="cart-item__icon" width={11} height={11}>
+									<use xlinkHref="/img/icons/sprites.svg#plus"></use>
+								</svg>
+							</button>
+						</article>
+					</div>
+
+					<div className="total">
+						<ul className="total__info">
+							<li className="total__item">
+								<span className="total__text">Итого:</span>
+								<div className="total__dashed"></div>
+								<span className="total__price">21 498 руб.</span>
+							</li>
+							<li className="total__item">
+								<span className="total__text">Налог 5%:</span>
+								<div className="total__dashed"></div>
+								<span className="total__price">1074 руб.</span>
+							</li>
+						</ul>
+						<button className="button">
+							<span className="button__text">Оформить заказ</span>
+							<svg className="button__icon" width={16} height={14}>
+								<use xlinkHref="/img/icons/sprites.svg#arrow"></use>
+							</svg>
+						</button>
+					</div>
+				</div>
+			</div>
+
 			<header className="header">
-				<a className="logo">
+				<a className="logo" href="/">
 					<img className="logo__img" width={40} height={40} src="/img/icons/logo.png" alt="Logo" />
 					<div className="logo__text">
 						<h3 className="logo__title">React Sneakers</h3>
@@ -30,12 +91,12 @@ function App() {
 				<div className="content">
 					<div className="content__header">
 						<h1 className="title">Все кроссовки</h1>
-						<div className="search">
+						<label className="search">
 							<svg className="search__icon" width={14} height={14}>
 								<use xlinkHref="/img/icons/sprites.svg#search"></use>
 							</svg>
 							<input className="search__input" type="text" placeholder="Поиск..." />
-						</div>
+						</label>
 					</div>
 
 					<div className="catalog">
@@ -53,7 +114,7 @@ function App() {
 									<span className="card__number">12 999 руб.</span>
 								</div>
 								<button className="card__button">
-									<svg className="card__icon card__icon_plus" width={10} height={10}>
+									<svg className="card__icon card__icon_plus" width={11} height={11}>
 										<use xlinkHref="/img/icons/sprites.svg#plus"></use>
 									</svg>
 								</button>

f539a0fb939ea81d947ffc7955f5944ae0cee70c
merpich <merpich.dev@gmail.com>
2022-10-17 23:33:57 +0300

    добавлено активное состояние иконки избранного

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,16 +3,31 @@ import './Card.scss';
 
 function Card({ title, imageUrl, price, onLike, onPlus }) {
 	const [isAdded, setIsAdded] = useState(false);
+	const [isFavourite, setIsFavourite] = useState(false);
 
 	const handlePlus = () => {
 		onPlus({title, imageUrl, price});
 		setIsAdded(!isAdded);
 	}
 
+	const onCliclkFavourite = () => {
+		setIsFavourite(!isFavourite);
+	}
+
 	return (
 		<article className="card">
-			<button className="card__button card__button_like" onClick={onLike}>
-				<svg className="card__icon card__icon_like" width={15} height={15}>
+			<button
+				className={isFavourite
+					? 'card__button card__button_like card__button_like_active'
+					: 'card__button card__button_like'}
+				onClick={onCliclkFavourite}
+			>
+				<svg
+					className={isFavourite
+						? 'card__icon card__icon_like card__icon_like_active'
+						: 'card__icon card__icon_like'}
+					width={15}
+					height={15}>
 					<use xlinkHref="/img/icons/sprites.svg#heart"></use>
 				</svg>
 			</button>
@@ -25,8 +40,8 @@ function Card({ title, imageUrl, price, onLike, onPlus }) {
 				</div>
 				<button
 					className={isAdded
-								? 'card__button card__button_checked'
-								: 'card__button'}
+						? 'card__button card__button_checked'
+						: 'card__button'}
 					onClick={handlePlus}>
 					<svg
 						className={isAdded
